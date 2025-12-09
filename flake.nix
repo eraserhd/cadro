26c7e29402d949b2713bb77521fde396762de51f
Jason Felice <jason.m.felice@gmail.com>
2025-12-09 15:42:00 -0500

    Make APK build work

--- flake.nix
+++ flake.nix
@@ -14,19 +14,26 @@
         };
 
         androidComposition = pkgs.androidenv.composeAndroidPackages {
-          platformVersions = [ "34" ];
-          buildToolsVersions = [ "34.0.0" ];
+          platformVersions = [ "34" "35" "36" ];
+          buildToolsVersions = [ "34.0.0" "35.0.0" ];
           includeEmulator = false;
         };
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            clojure
-            androidComposition.androidsdk
-          ];
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              clojure
+              nodejs
+              jdk21
 
-          ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
-          ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+              androidComposition.androidsdk
+            ];
+            ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
+            ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
+            JAVA_HOME = "${pkgs.jdk21}";
+            GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidComposition.androidsdk}/libexec/android-sdk/build-tools/35.0.0/aapt2";
+          };
         };
     }));
 }
+
