fc04b25385b2014ebb70205f451bfff69cf44d2f
Jason Felice <jason.m.felice@gmail.com>
2025-12-25 11:28:25 -0500

    Keep database stable for debugging

--- src/cadro/db.cljs
+++ src/cadro/db.cljs
@@ -1,24 +1,33 @@
 (ns cadro.db
   (:require
    [datascript.core :as d]
+   [datascript.conn :as dconn]
    [re-posh.core :as re-posh]))
 
+(defonce ^:dynamic *conn* nil)
 (def ^:private schema-atom (atom {}))
-
-(defn register-schema! [subschema]
-  (swap! schema-atom merge subschema))
 
 (defn schema
   "Retrieves all registered schema."
   []
   @schema-atom)
 
-(def ^:dynamic *conn* nil)
+(defn register-schema!
+  "Registers some partial DataScript schema for model attributes."
+  [subschema]
+  (swap! schema-atom merge subschema)
+  (when *conn*
+    (dconn/reset-schema! *conn* @schema-atom))
+  nil)
 
 (defn connect!
-  "Creates a database connection with all registered schema."
+  "Creates a database connection with all registered schema.
+
+  Tries to reuse an existing connection for database stability when running
+  in debug mode."
   []
-  (let [conn (d/create-conn (schema))]
-    (re-posh/connect! conn)
-    (set! *conn* conn)
-    conn))
+  (when-not *conn*
+    (let [conn (d/create-conn (schema))]
+      (re-posh/connect! conn)
+      (set! *conn* conn)))
+  *conn*)
