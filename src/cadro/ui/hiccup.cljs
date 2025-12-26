3dcb7e2b83fad965e43b39792ebbaa4a3dd26ba3
Jason Felice <jason.m.felice@gmail.com>
2025-12-26 12:18:46 -0500

    Merge props when wrapping content

--- src/cadro/ui/hiccup.cljs
+++ src/cadro/ui/hiccup.cljs
@@ -1,5 +1,10 @@
-(ns cadro.ui.hiccup)
+(ns cadro.ui.hiccup
+  (:require
+   [reagent.core :as r]))
 
 (defn wrap-content
   [props content]
-  content)
+  (if-let [[tag existing-props & body] content]
+    (into [tag (r/merge-props existing-props props)]
+          body)
+    content))
--- src/cadro/ui/hiccup_test.cljs
+++ src/cadro/ui/hiccup_test.cljs
@@ -1,9 +1,10 @@
 (ns cadro.ui.hiccup-test
   (:require
    [cadro.ui.hiccup :as h]
-   [clojure.test :refer [deftest testing is]]))
+   [clojure.test :refer [deftest testing are]]))
 
 (deftest t-wrap-content
   (testing "merging props"
-    (is (= [:foo {} [:bar]]
-           (h/wrap-content {} [:foo {} [:bar]])))))
+    (are [expect props input] (= expect (h/wrap-content props input))
+      [:a {:b "f"} [:c]]         {}       [:a {:b "f"} [:c]]
+      [:a {:b "f", :d 42} [:c]]  {:d 42}  [:a {:b "f"} [:c]])))
