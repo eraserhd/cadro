bcb484e824949e99778938d78163298a17754a1c
Jason Felice <jason.m.felice@gmail.com>
2025-12-26 12:34:06 -0500

    Normalize

--- src/cadro/ui/hiccup.cljs
+++ src/cadro/ui/hiccup.cljs
@@ -2,6 +2,14 @@
   (:require
    [reagent.core :as r]))
 
+(defn normalize
+  [content]
+  (let [[tag props & more] content]
+    (cond
+     (map? props)                   (into [tag props] more)
+     (and (nil? props) (nil? more)) [tag {}]
+     :else                          (into [tag {} props] more))))
+
 (defn wrap-content
   [props content]
   (if-let [[tag existing-props & body] content]
--- src/cadro/ui/hiccup_test.cljs
+++ src/cadro/ui/hiccup_test.cljs
@@ -3,9 +3,17 @@
    [cadro.ui.hiccup :as h]
    [clojure.test :refer [deftest testing are]]))
 
+(deftest t-normalize
+  (are [input expect] (= expect (h/normalize input))
+      [:a]              #_=> [:a {}]
+      [:a {}]           #_=> [:a {}]
+      [:a [:b]]         #_=> [:a {} [:b]]
+      [:a {:d :f} "t"]  #_=> [:a {:d :f} "t"]))
+
 (deftest t-wrap-content
   (testing "merging props"
     (are [expect props input] (= expect (h/wrap-content props input))
       [:a {:b "f"} [:c]]         {}       [:a {:b "f"} [:c]]
       [:a {:b "f", :d 42} [:c]]  {:d 42}  [:a {:b "f"} [:c]]
       [:a {:b "q"} [:c]]         {:b "q"} [:a {:b "f"} [:c]])))
+      ;[:a {:b "q"} [:c]]         {:b "q"} [:a [:c]])))
