4b4d91f6aedd8eedb6cc5b8bf16110f3ce8d6a35
Jason Felice <jason.m.felice@gmail.com>
2026-01-05 10:32:37 -0500

    Simplify

--- src/cadro/transforms.cljs
+++ src/cadro/transforms.cljs
@@ -2,13 +2,11 @@
   (:refer-clojure :exclude [-]))
 
 (defn- pairwise
-  [f a b default]
+  [f default a b]
   (->> (concat (keys a) (keys b))
        distinct
        (map (fn [k]
               [k (f (get a k default) (get b k default))]))
        (into {})))
 
-(defn -
-  [a b]
-  (pairwise clojure.core/- a b 0))
+(def - (partial pairwise clojure.core/- 0))
