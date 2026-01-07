bb2e85ee8c09f587d9a49327ab766f398ea13e5f
Jason Felice <jason.m.felice@gmail.com>
2026-01-07 16:31:44 -0500

    Persist session facts

--- src/cadro/session.cljs
+++ src/cadro/session.cljs
@@ -2,13 +2,22 @@
   (:require-macros
    [clara.rules :as clara])
   (:require
-   [cadro.model]
+   [cadro.model :as model]
    [clara.rules :as clara]
+   [clojure.edn :as edn]
    [re-frame.core :as r]))
 
 (clara/defsession empty-session 'cadro.model)
 
-(defonce session (atom empty-session))
+(defonce session (atom
+                   (let [ls-tuples (some-> js/localStorage
+                                     (.getItem "session")
+                                     (edn/read-string))]
+                     (-> empty-session
+                         (clara/insert-all (map (fn [[e a v]]
+                                                  (model/asserted e a v))
+                                                ls-tuples))
+                         (clara/fire-rules)))))
 
 (defn clear! []
   (reset! session empty-session))
@@ -16,7 +25,12 @@
 (r/reg-fx
  :session
  (fn [new-session]
-   (reset! session (clara/fire-rules new-session))))
+   (let [new-session (clara/fire-rules new-session)
+         tuples      (->> (clara/query new-session model/persistent-facts)
+                          (map (fn [{:keys [?e ?a ?v]}]
+                                 [?e ?a ?v])))]
+     (reset! session new-session)
+     (.setItem js/localStorage "session" (pr-str tuples)))))
 
 (r/reg-cofx
  :session
